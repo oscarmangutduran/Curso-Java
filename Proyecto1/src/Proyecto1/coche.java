ec9a8d6241c7235189c94b60d5f078c2c4b06952
Oscar Mangut <mangutduranoscar@gmail.com>
2024-07-31 20:22:26 +0200

    Primer Programa

---
+++ Proyecto1/src/Proyecto1/coche.java
@@ -0,0 +1,30 @@
+package Proyecto1;
+
+public class Coche {
+    
+    public Coche() {
+        // Constructor de objetos
+        System.out.println("Se ha construido el objeto");
+    }
+
+    // Métodos
+    public void avanzar() {
+        System.out.println("Estoy avanzando");
+    }
+
+    public void frenar() {
+        System.out.println("Estoy frenando");
+    }
+
+    public void arrancar() {
+        System.out.println("Estoy arrancando el coche");
+    }
+
+    public void apagar() {
+        System.out.println("Estoy apagando el coche");
+    }
+
+    public void gasolina() {
+        System.out.println("Con el precio de la gasolina es imposible poder tener gasolina");
+    }
+}
---
+++ Proyecto1/src/Proyecto1/proyecto1.java
@@ -0,0 +1,11 @@
+package Proyecto1;
+
+public class Proyecto1 {
+    public static void main(String[] args) {
+        Coche ferrari = new Coche();
+
+        ferrari.encender();
+        ferrari.gasolina();
+        ferrari.apagar();
+    }
+}
