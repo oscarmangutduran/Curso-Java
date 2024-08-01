353000cf3104bd5c67515e3ca54fc17ea034301e
Oscar Mangut <mangutduranoscar@gmail.com>
2024-08-01 21:14:19 +0200

    Primer video

---
+++ primerVideo/src/primerVideo/PrimerVideo.java
@@ -0,0 +1,14 @@
+package primerVideo;
+
+public class PrimerVideo {
+	
+	public static void main(String[] args) {
+		
+		coche ferrari = new coche();
+		
+		ferrari.arrancar();
+		ferrari.gasolina();
+		ferrari.apagar();
+	}
+
+}
---
+++ primerVideo/src/primerVideo/coche.java
@@ -0,0 +1,36 @@
+package primerVideo;
+
+public class coche {
+	
+	public coche() {
+		
+		//constructor
+		
+		System.out.println("Se ha construido el objeto");
+	}
+	
+	//métodos
+	
+	public void avanzar() {
+		System.out.println("Estoy avanzando");
+	}
+	
+	public void frenar() {
+		System.out.println("Estoy frenando");
+	}
+	
+	public void arrancar() {
+		System.out.println("Estoy arrancar");
+	}
+	
+	public void apagar() {
+		System.out.println("Estoy apagando");
+	}
+	
+	public void gasolina() {
+		System.out.println("No puedo encender tu coche no tienes gasolina");
+	}
+	
+	
+
+}
